e4dd76eced836d97a65f7d523fa3cc9366775b47
Emilio Orduña <emilio.ordunap@hotmail.com>
2024-06-01 15:17:35 +0200

    delete unnecessary component user-table

--- src/app/data-table/data-table.module.ts
+++ src/app/data-table/data-table.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 
 import { DataTableRoutingModule } from './data-table-routing.module';
-import { UsersPageComponent } from './pages/users-page/users-page.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    UsersPageComponent
   ],
   imports: [
     CommonModule,
